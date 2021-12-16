ee0c753c3e9f5a4a3aee790eb2a1566d0fc14f54
Rossetti <jules.rossetti@aol.fr>
2021-12-16 22:42:57 +0100

    Born2beroot

--- monitoring.sh
+++ monitoring.sh
@@ -3,11 +3,18 @@
 total=$(free -m | awk 'NR==2 {print $2}')
 totoro=$(free -m | awk 'NR==2 {print $3}')
 toto=$((100 * $totoro / $total))
+tcp=$(who | wc -l)
+pourcent=$(uptime | awk '{print $10}' | tr -d ,)
 
 echo -n "#Architecture: "; uname -a
 echo -n "#CPU physical : "; cat /proc/cpuinfo | grep 'physical id' | uniq | wc -l
 echo -n "#vCPU : "; cat /proc/cpuinfo | grep 'cpu cores' | uniq | awk '{print $4}'
 echo  	"#Memory Usage: $totoro/${total}MB ($toto%)"
 echo    "#Disk Usage: "
-echo -n "#CPU load: "; uptime | awk '{print $10}' | tr -d ,
-echo -n "#Last boot: ";  
+echo    "#CPU load: $pourcent%"
+echo -n "#Last boot: "; who -b | awk '{print$3,$4}'
+echo    "#LVM use: "
+echo -n "#Connexions TCP : "; cat /proc/net/sockstat | awk 'NR==2 {print$3}'
+echo    "#User log: $tcp ESTABLISHED"
+echo -n "#Network: " 
+echo -n "#Sudo : "
