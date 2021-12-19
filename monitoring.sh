825e4769de699c79a0388fdb6fc715ab44ec4a10
Rossetti Jules <68472822+Teambersaw@users.noreply.github.com>
2021-12-19 15:13:07 +0100

    Born2beroot

--- monitoring.sh
+++ monitoring.sh
@@ -9,27 +9,33 @@ Memory=$(free -m | awk 'NR==2 {print $3}')
 Max_Memory=$(free -m | awk 'NR==2 {print $2}')
 Total=$((100 * $Memory / $Max_Memory))
 
+Disk_Usage=$()
+
 CPU_Load=$(uptime | awk '{print $10}' | tr -d ,)
 
 Last_Boot=$(who -b | awk '{print$3,$4}')
+
+LVM_use=$()
 
 Connexions_TCP=$(cat /proc/net/sockstat | awk 'NR==2 {print$3}')
 
 User_log=$(who | wc -l)
 
-Network=$(hostname -I)
+Ipv4=$(hostname -I)
+MAC=$(ip link show | awk 'NR==4 {print $2}')
 
+Sudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
 echo   "
         #Architecture: $Architecture
         #CPU physical : $CPU_physical
         #vCPU : $vCPU
         #Memory Usage: $Memory/${Max_Memory}MB ($Total%)
-        #Disk Usage:
+        #Disk Usage: $Disk_Usage
         #CPU load: $CPU_Load%
         #Last boot: $Last_Boot
-        #LVM use:
+        #LVM use: $LVM_use
         #Connexions TCP : $Connexions_TCP ESTABLISHED
         #User log: $User_log
-        #Network: IP $Network
-        #Sudo :"
+        #Network: IP $Ipv4 ($MAC)
+        #Sudo : $Sudo cmd
